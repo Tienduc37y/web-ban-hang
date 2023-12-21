de5e0522c41d292de51ec5f76ba0e283d59e6485
Tienduc37y <laitienduc37@gmail.com>
2023-12-21 08:50:10 +0700

    header

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import { useState } from 'react'
 import { UseFetch } from './common/UseFetch'
 import "./App.css"
 import { Link } from 'react-router-dom'
+import Header from './component/Header/Header'
 function App() {
   const {product,setProduct,paging,setPaging} = UseFetch()
   return (
     <>
+      <Header></Header>
       <div className='product'>
         {product?.map(item=>{
           const url = `https://backoffice.nodemy.vn` + item?.attributes?.image?.data[0]?.attributes?.url
           return (
             <Link to={`/${item?.attributes?.slug}`} key={item?.id}>
               <div className='product-item'>
-                <h5>{item?.attributes?.name}</h5>
+                <h5 className='product-name'>{item?.attributes?.name}</h5>
                 <img src={url} alt="" style={{
                   width:200,
                   height:200
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,8 +1,19 @@
 import './style.css'
+import { Link } from 'react-router-dom'
 export default function Header(){
     return (
         <>
-
+            <div className='header'>
+                <div className='logo-shop'>
+                    <Link to="/">
+                        <img src="src\assets\—Pngtree—shop logo design with a_7961653.png" alt="" />
+                    </Link>
+                </div>
+                <div className='user'>
+                    <Link to="/login" className='link'>Đăng nhập</Link>
+                    <Link to="/register" className='link'>Đăng ký</Link>
+                </div>
+            </div>
         </>
     )
 }
